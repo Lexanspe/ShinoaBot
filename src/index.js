bbb348ed7797c64b17b3d6b8770ab9d7aa45cacb
Lexanspe <arifalperenesenkar2019@gmail.com>
2024-05-26 04:01:37 +0300

    added gif2someone and developer mode

--- src/index.js
+++ src/index.js
@@ -32,8 +32,27 @@ client.on("interactionCreate", async (interaction) => {
         let url = `https://tenor.googleapis.com/v2/search?q=${interaction.options.getString('character')}&key=${process.env.TENORKEY}&limit=50`
         let response = await fetch(url);
         let json = await response.json();
-        interaction.reply(json.results[Math.floor(Math.random() * json.results.length)].url);
-        console.log(`Sent gif for ${interaction.options.getString('character')} by ${interaction.user.tag}`);
+        let number = Math.floor(Math.random() * json.results.length);
+        if (interaction.options.getBoolean('developermode')) {
+            if (interaction.user.id != process.env.OWNERID) { interaction.reply({ content: "You do not have permission to use developer.", ephemeral: true }); return;}
+            console.log("Developer mode activated");
+            await interaction.reply(json.results[0].url);
+            for (let i = 1; i < json.results.length; i++) {
+                interaction.followUp(json.results[i].url);
+            }
+        } else {
+
+            if (interaction.options.getInteger('sgn') != null) {
+                if (interaction.options.getInteger('sgn') > 50) {
+                    interaction.reply({ content: "There are only 50 gifs available. Sorry!", ephemeral: true });
+                    return;
+                }
+                number = interaction.options.getInteger('sgn') - 1;
+            }
+            interaction.reply(json.results[number].url);
+            console.log(`Sent gif for ${interaction.options.getString('character')} by ${interaction.user.tag}, gif number: ${number}, selected by user: ${interaction.options.getInteger('sgn') != null}`);
+        }
+        
     }
     else if (commandName === "setbanner") {
         if (interaction.user.id != process.env.OWNERID) {
@@ -50,6 +69,29 @@ client.on("interactionCreate", async (interaction) => {
             interaction.reply({ content: "An error occurred while setting the banner.", ephemeral: true });
             return;
         }
+    }
+    else if (commandName === "gif2someone") {
+        if (interaction.options.getString('action') == "rickroll") {
+            interaction.reply({ content: "nope, no rickroll", ephemeral: true });
+            console.log("rickroll");
+            return;
+        }
+        let url = `https://tenor.googleapis.com/v2/search?q=${interaction.options.getString('action')}+anime&key=${process.env.TENORKEY}&limit=20`
+        let response = await fetch(url);
+        let json = await response.json();
+        let number = Math.floor(Math.random() * json.results.length);
+        if (interaction.options.getInteger('sgn') != null) {
+            if (interaction.options.getInteger('sgn') > 20) {
+                interaction.reply({ content: "There are only 20 gifs available. Sorry!", ephemeral: true });
+                return;
+            }
+            number = interaction.options.getInteger('sgn') - 1;
+        }
+        interaction.guild.members.fetch(interaction.options.getUser('user')).then((user) => {
+            interaction.reply(json.results[number].url);
+            console.log(`Sent gif for ${interaction.options.getString('action')} to ${interaction.options.getUser('user').tag} by ${interaction.user.tag}, gif number: ${number}, selected by user: ${interaction.options.getInteger('sgn') != null}`);
+            //interaction.reply({ content: `Sent gif to ${user.tag}` });
+        });
     }
 });
 
--- src/register-commands.js
+++ src/register-commands.js
@@ -11,9 +11,51 @@ const commands = [
                 description: 'Choose a character',
                 type: 3, 
                 required: true,
+            },
+            {
+                name: 'sgn',
+                description: 'Stands for "Specific Gif Number". If you want a specific gif, type the number here',
+                type: 4, 
+                required: false,
+            },
+            {
+                name: 'developermode',
+                description: 'for developers only',
+                type: 5, 
+                required: false,
             }
         ]
     },
+    {
+        name: 'gif2someone',
+        description: 'Sends a random gif of what you choose to someone',
+        options: [
+            {
+                name: 'action',
+                description: 'Choose a action',
+                type: 3, 
+                required: true,
+                choices: [{name: 'cuddle', value: 'cuddle'}, {name: 'hug', value: 'hug'}, {name: 'kiss', value: 'kiss'}, {name: 'lick', value: 'lick'}, {name: 'nom', value: 'nom'}, {name: 'pat', value: 'pat'}, {name: 'poke', value: 'poke'}, {name: 'slap', value: 'slap'}, {name: 'stare', value: 'stare'}, {name: 'highfive', value: 'highfive'}, {name: 'bite', value: 'bite'}, {name: 'greet', value: 'greet'}, {name: 'punch', value: 'punch'}, {name: 'handholding', value: 'handholding'}, {name: 'tickle', value: 'tickle'}, {name: 'kill', value: 'kill'}, {name: 'hold', value: 'hold'}, {name: 'pats', value: 'pats'}, {name: 'wave', value: 'wave'}, {name: 'boop', value: 'boop'}, {name: 'snuggle', value: 'snuggle'}, {name: 'bully', value: 'bully'}]
+                //['cuddle', 'hug', 'kiss', 'lick', 'nom', 'pat', 'poke', 'slap', 'stare', 'highfive', 'bite', 'greet', 'punch', 'handholding', 'tickle', 'kill', 'hold', 'pats', 'wave', 'boop', 'snuggle', 'bully']
+            },
+            {
+                name: 'user',
+                description: 'Mention the user you want to send the gif to',
+                type: 6, 
+                required: true,
+            },
+            {
+                name: 'sgn',
+                description: 'Stands for "Specific Gif Number". If you want a specific gif, type the number here',
+                type: 4, 
+                required: false,
+            }
+        ]
+    },
+
+
+
+
     {
         name: 'setbanner',
         description: 'sets banner',
